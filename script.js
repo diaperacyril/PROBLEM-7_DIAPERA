b2a822e96cd46613f6379ef816aeee96b060d0a2
Your Name <your-email@gmail.com>
2026-04-29 20:25:23 +0800

    i  implement and  add plant feature with validation and reset inputs

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 let plants = JSON.parse(localStorage.getItem("plants")) || [];
 function saveData() {
     localStorage.setItem("plants", JSON.stringify(plants));
+} 
+function addPlant() {
+    const name = document.getElementById("plantName").value;
+    const frequency = document.getElementById("frequency").value;
+
+    if (!name || !frequency) {
+        alert("Please fill all fields");
+        return;
+    }
+
+    const today = new Date().toISOString();
+
+    plants.push({
+        name,
+        frequency: parseInt(frequency),
+        lastWatered: today
+    });
+
+    saveData();
+    displayPlants();
+
+    document.getElementById("plantName").value = "";
+    document.getElementById("frequency").value = "";
 }
